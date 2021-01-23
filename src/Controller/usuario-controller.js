22efd054a9109a32f5c6d72012411562c73274fe
Scarlate <scarlate100@gmail.com>
2021-01-23 18:20:11 -0300

    implementação verbo delete

--- src/Controller/usuario-controller.js
+++ src/Controller/usuario-controller.js
@@ -21,34 +21,24 @@ module.exports = (app, bd) => {
         console.log(bd.usuario)
         res.send("adicionado com sucesso")
             // console.log(req.body)
-            // res.send(`Rota ativada com POST e recurso usuario: valores de usuario devem ser retornados`)
     })
 
     app.delete('/usuario/:nome', (req, res) => {
-        for (let usr of bd.usuario) {
-            if (req.params.nome === usr.nome) {
+        for (let usr = 0; usr < bd.usuario.length; usr++) {
+            if (req.params.nome === bd.usuario[usr].nome) {
                 bd.usuario.splice(usr, 1)
                 console.log()
                 res.send("removido com sucesso")
-
             }
-
-
-
         }
-
     })
 
     app.put('/usuario/:nome', (req, res) => {
         for (let usr of bd.usuario) {
             if (usr.nome === req.params.nome) {
                 usr.nome = req.body.nome
-
                 return res.json({ message: 'atualizado ' })
             }
         }
-
-
     })
-
 }
