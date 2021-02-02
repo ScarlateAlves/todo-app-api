ea61803ddc5b02f3e86aade78b02ba5ba3c1e547
Scarlate <scarlate100@gmail.com>
2021-02-02 14:58:04 -0300

    Utilizando async no projeto

--- src/Controller/usuario-controller.js
+++ src/Controller/usuario-controller.js
@@ -4,41 +4,56 @@ const dao = require('../DAO/usuario-dao')
 module.exports = (app, bd) => {
     const usuarioDao = new dao(bd)
 
-    app.get('/usuario', (req, res) => {
-        usuarioDao.listaUsuarios()
-            .then((usuarios) => { res.send(usuarios) })
-            .catch((erro) => { res.send(erro) })
+    app.get('/usuario', async(req, res) => {
+        try {
+            let resposta = await usuarioDao.listaUsuarios()
+            res.status(200).send(resposta)
+        } catch (erro) {
+            res.status(400).send(erro)
+        }
     })
 
     //pode ser usado o each tbm
-    app.get('/usuario/:id', (req, res) => {
-        let parametro = req.params.id;
-        usuarioDao.buscaUsuario(parametro)
-            .then((usuarios) => { res.send(usuarios) })
-            .catch((erro) => { res.send(erro) })
+    app.get('/usuario/:id', async(req, res) => {
+        try {
+            let parametro = req.params.id;
+            let resposta = await usuarioDao.buscaUsuario(parametro)
+            res.status(200).send(resposta)
+        } catch (erro) {
+            res.status(400).send(erro)
+        }
 
     })
 
-    app.post(`/usuario`, (req, res) => {
-        let parametro = [req.body.nome, req.body.email, req.body.senha];
-        usuarioDao.adicionaUsuario(parametro)
-            .then((usuarios) => { res.status(200).send(usuarios) })
-            .catch((erro) => { res.status(400).send(erro) })
+    app.post(`/usuario`, async(req, res) => {
+        try {
+            let parametro = [req.body.nome, req.body.email, req.body.senha];
+            let resposta = await usuarioDao.adicionaUsuario(parametro)
+            res.status(200).send(resposta)
+        } catch (erro) {
+            res.status(400).send(erro)
+        }
     })
 
-    app.delete('/usuario/:id', (req, res) => {
-        let parametro = req.params.id;
-        usuarioDao.deletarUsuario(parametro)
-            .then((usuarios) => { res.send(usuarios) })
-            .catch((erro) => { res.send(erro) })
+    app.delete('/usuario/:id', async(req, res) => {
+        try {
+            let parametro = req.params.id;
+            let resposta = await usuarioDao.deletarUsuario(parametro)
+            res.status(200).send(resposta)
+        } catch (erro) {
+            res.status(400).send(erro)
+        }
 
     })
 
-    app.put('/usuario/:id', (req, res) => {
-        let parametro = [req.body.nome, req.body.email, req.body.senha, req.params.id]
-        usuarioDao.alteraUsuario(parametro)
-            .then((usuarios) => { res.send(usuarios) })
-            .catch((erro) => { res.send(erro) })
+    app.put('/usuario/:id', async(req, res) => {
+        try {
+            let parametro = [req.body.nome, req.body.email, req.body.senha, req.params.id]
+            let resposta = await usuarioDao.alteraUsuario(parametro)
+            res.status(200).send(resposta)
+        } catch (erro) {
+            res.status(400).send(erro)
+        }
     })
 
 }
